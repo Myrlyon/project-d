ed8f695d8d0dafaf952dab1af2c21d165a03f828
Marlen Satriani <marlen7891011@gmail.com>
2024-06-13 21:50:18 +0700

    style: [ME] Minor style fix

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -4,48 +4,48 @@ import mainLogo from "../../assets/images/Project_d_logo.png";
 
 const Home = () => {
   return (
-    <div>
-      <div className="flex items-center justify-center pt-12">
+    <div className="px-[250px]">
+      <div className="flex justify-center pt-20">
         <img
           src={mainLogo}
           height={100}
           width={1000}
           alt="Main Logo"
-          className="items-center"
+          className="items-center float-start"
         />
       </div>
-      <div className="pt-[100px] pl-[250px]">
+      <div className="pt-[100px]">
         <ul className="space-y-10">
           <li>
-            <a className=" text-white text-2xl" href="#bottom">
+            <a className="hover:underline text-white text-2xl" href="#bottom">
               治ス府英
             </a>
           </li>
           <li>
-            <a className=" text-white text-2xl" href="#bottom">
-              治ス府英レづ問載や
+            <a className="hover:underline text-white text-2xl" href="#bottom">
+              事省ッ島懸ぞどずさ旬行
             </a>
           </li>
           <li>
-            <a className=" text-white text-2xl" href="#bottom">
-              治ス府英
+            <a className="hover:underline text-white text-2xl" href="#bottom">
+              ふぽ的選医わ
             </a>
           </li>
           <li>
-            <a className=" text-white text-2xl" href="#bottom">
-              治ス府英
+            <a className="hover:underline text-white text-2xl" href="#bottom">
+              ぼそ妃球気
             </a>
           </li>
           <li>
-            <a className=" text-white text-2xl" href="#bottom">
-              治ス府英
+            <a className="hover:underline text-white text-2xl" href="#bottom">
+              おひ声座ネ
             </a>
           </li>
         </ul>
         <ul className="pt-[100px]">
           <li>
-            <a className=" text-white text-2xl" href="#bottom">
-              治ス府英
+            <a className="hover:underline text-white text-2xl" href="#bottom">
+              現購権ゆッ
             </a>
           </li>
         </ul>
